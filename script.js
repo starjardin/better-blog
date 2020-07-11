161ac27df77483911cd69cc79f51b591d97f7665
Tantely Andrianarivola <tantely.and@onja.org>
2020-07-11 11:25:17 +0300

    show and hide the form

--- script.js
+++ script.js
@@ -4,36 +4,53 @@ const submitButton = document.querySelector('.btn-primary');
 const postTitle = document.getElementById('postTitle');
 const postContent = document.querySelector(`[name= "postContent"]`);
 const postAuthor = document.querySelector(`[name = 'postAuthor']`);
-// Function to generate new posts
-const createNewPost = () => {
-  const cardForm = document.querySelector('#form-card');
-  const cardBody = document.querySelector('.card-body');
-  const myPost = `
-  <h5 class="card-title">${postTitle.value}
-    <small>${postAuthor.value}</small>
-  </h5>
-  <p class="card-text">${postContent.value}</p>
-  `
-  const myFragment = document.createRange().createContextualFragment(myPost);
-  cardBody.appendChild(myFragment);
-  cardForm.appendChild(cardBody);
-};
+const showForm = document.querySelector('#show-form');
+const postForm = document.querySelector('#post-form');
+const collection = document.getElementsByClassName('col-8 offset-2');
+console.log(collection)
+
+// Add an event listener here
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
-  const container = document.querySelector('.container');
-  const postList = document.querySelector('#post-list');
-  const myPost = `
-  <div class="card">
-    <img class="card-img-top" src="https://picsum.photos/500/200" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">${postTitle.value}
-        <small>${postAuthor.value}</small>  
-      </h5>
-      <p class="card-text">${postContent.value}</p>
-    </div>
-  </div>`
-  const myFragment = document.createRange().createContextualFragment(myPost);
-  postList.prepend(myFragment);
-  container.prepend(postList);
-  console.log(container)
+  if (postContent.value.length < 20) {
+    postContent.classList.add('is-invalid');
+    const invalidDiv = document.querySelector('.card-body');
+    invalidDiv.classList.add('invalid-feedBack');
+  } else {
+    postContent.classList.remove('is-invalid');
+    const invalidDiv = document.querySelector('.card-body');
+    invalidDiv.classList.remove('invalid-feedBack');
+    const container = document.querySelector('.container');
+    const postList = document.querySelector('#post-list');
+    const myPost = `
+    <div class="card">
+      <img class="card-img-top" src="https://picsum.photos/500/200" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${postTitle.value}
+          <small> By ${postAuthor.value}</small>  
+        </h5>
+        <p class="card-text">${postContent.value}</p>
+        <button type="button" class="btn btn-sm btn-light btn-delete">
+          Delete entry
+        </button>
+      </div>
+      <div class="card-footer text-muted">
+        10/07/2020
+      </div>
+    </div>`
+    const myFragment = document.createRange().createContextualFragment(myPost);
+    postList.prepend(myFragment);
+    container.prepend(postList);
+  };
+  myForm.reset();
+})
+
+showForm.addEventListener('click', (e) => {
+  if (showForm.textContent === "Hide form") {
+    postForm.classList.add('hidden');
+    showForm.textContent = "show form";
+  } else {
+    postForm.classList.remove('hidden');
+    showForm.textContent = "Hide form"
+  }
 })
