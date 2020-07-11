24755a635d07c95fe9724b2cc585ab05f7d87b94
Tantely Andrianarivola <tantely.and@onja.org>
2020-07-11 16:21:36 +0300

    renamed class name

--- script.js
+++ script.js
@@ -9,49 +9,54 @@ const collection = document.getElementsByClassName('col-8 offset-2');
 const postList = document.querySelector('#post-list');
 const postCard = document.querySelector('.card');
 const formCard = document.querySelector('#form-card');
+const imageSource = document.querySelector(`[name=postImg]`);
 
 
 // Add an event listener here
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
-  if (postContent.value.length < 20) {
-    postContent.classList.add('is-invalid');
-    const invalidDiv = document.querySelector('.card-body');
-    invalidDiv.classList.add('invalid-feedBack');
+  if (postContent === '' || postTitle === '' || postAuthor === '') {
+    // submitButton.setAttribute('disabled'= "true");
   } else {
-    postContent.classList.remove('is-invalid');
-    const invalidDiv = document.querySelector('.card-body');
-    invalidDiv.classList.remove('invalid-feedBack');
-    const container = document.querySelector('.container');
-//Create a new element post.
-    const myPost = `
-    <div class="card">
-      <img class="card-img-top" src="https://picsum.photos/500/200" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${postTitle.value}
-          <small> By ${postAuthor.value}</small>  
-        </h5>
-        <p class="card-text">${postContent.value}</p>
-        <button type="button" class="btn btn-sm btn-light btn-delete">
-          Delete entry
-        </button>
-      </div>
-      <div class="card-footer text-muted">
-        10/07/2020
-      </div>
-    </div>`
-    const myFragment = document.createRange().createContextualFragment(myPost);
-    postList.prepend(myFragment);
-    container.prepend(postList);
-    const deleteButton = document.querySelectorAll('.btn-delete');
-//Delete a post from the dom.
-    for (let i = 0; i < deleteButton.length; i++) {
-      deleteButton[i].addEventListener('click', (e) => {
-        const cardDele = deleteButton[i].parentElement.parentElement;
-        cardDele.style.display = 'none'
-      });
-    }
-  };
+    if (postContent.value.length < 20) {
+      postContent.classList.add('is-invalid');
+      const invalidDiv = document.querySelector('.card-body');
+      invalidDiv.classList.add('invalid-feedBack');
+    } else {
+      postContent.classList.remove('is-invalid');
+      const invalidDiv = document.querySelector('.card-body');
+      invalidDiv.classList.remove('invalid-feedBack');
+      const container = document.querySelector('.container');
+  //Create a new element post.
+      const myPost = `
+      <div class="card">
+        <img class="card-img-top" src="https://picsum.photos/500/200" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${postTitle.value}
+            <small> By ${postAuthor.value}</small>  
+          </h5>
+          <p class="card-text">${postContent.value}</p>
+          <button type="button" class="btn btn-sm btn-light btn-delete">
+            Delete entry
+          </button>
+        </div>
+        <div class="card-footer text-muted">
+          10/07/2020
+        </div>
+      </div>`
+      const myFragment = document.createRange().createContextualFragment(myPost);
+      postList.prepend(myFragment);
+      container.prepend(postList);
+      const deleteButton = document.querySelectorAll('.btn-delete');
+  //Delete a post from the dom.
+      for (let i = 0; i < deleteButton.length; i++) {
+        deleteButton[i].addEventListener('click', (e) => {
+          const cardDele = deleteButton[i].parentElement.parentElement;
+          cardDele.style.display = 'none'
+        });
+      }
+    };
+  }
   myForm.reset();
 })
 
